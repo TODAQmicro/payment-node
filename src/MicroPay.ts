c920a2e2e6624cd818bc455219b28683057cfb9a
Matthew Mihok <mihok@identbase.io>
2024-01-15 06:37:20 -0500

    feat: Get authentication when initializing the app

--- src/MicroPay.ts
+++ src/MicroPay.ts
@@ -1,5 +1,8 @@
+import { MicroOptions } from './types';
 import * as resources from './resources';
 // import { MicroOptions, MicroInterface } from './types';
+
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8500';
 
 // interfaceCall is a Symbol that represents what might be an interface in the
 //  SDK.
@@ -14,34 +17,61 @@ export type MicroInterfaceObject = {
 };
 
 export class Micro {
-  static VERSIONS = {
-    [Symbol('latest')]: 'main',
-    'main': 'main',
+  static VERSIONS: { [type: symbol | string]: string } = {
+    [Symbol('latest')]: 'v2',
+    'v2': 'v2',
   };
 
-  // private clientId?: string;
-  // private clientSecret?: string;
-  // private get credentials() { return btoa(`${this.clientId}:${this.clientSecret}`);  };
-  // public setCredentials(clientId: string, clientSecret: string) {
-  //   this.clientId = clientId;
-  //   this.clientSecret = clientSecret;
-  // }
+  private authenticated: Promise<void> | null = null;
 
-  // private version: symbol | string;
+  private clientId?: string;
+  private clientSecret?: string;
+  // private get credentials() { return btoa(`${this.clientId}:${this.clientSecret}`); };
+  private accessToken: string = "";
+  private refreshToken: string = "";
+
+  public async authCredentials(clientId: string, clientSecret: string): Promise<void> {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+
+    const response = await fetch(
+      `${API_BASE_URL}/${String(this.version)}/`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Basic ${Buffer.from(`${this.clientId}:${this.clientSecret}`).toString('base64')}`,
+        },
+      },
+    );
+
+    const { access_token, refresh_token } = (await response.json() as any);
+
+    this.accessToken = access_token;
+    this.refreshToken = refresh_token;
+  }
+
+  private v: symbol | string = Symbol('latest');
+  private get version() {
+    return Micro.VERSIONS[this.v];
+  }
+  private set version(value: symbol | string) {
+    this.v = value;
+  }
 
   constructor(
-    // clientId: string,
-    // clientSecret: string,
-    // {
-    //   apiVersion = process.env.NODE_ENV !== 'prod' &&
-    //   process.env.NODE_ENV !== 'produciton'
-    //     ? 'main'
-    //     : Symbol('latest'),
-    // }: MicroOptions,
+    clientId: string,
+    clientSecret: string,
+    {
+      apiVersion = process.env.NODE_ENV !== 'prod' &&
+      process.env.NODE_ENV !== 'produciton'
+        ? 'main'
+        : Symbol('latest'),
+    }: MicroOptions,
   ) {
-    // this.version = apiVersion;
-
-    // this.setCredentials(clientId, clientSecret);
+    this.version = apiVersion;
+    this.authenticated = this.authCredentials(clientId, clientSecret);
 
     const protectedKeys = Object.keys(this);
     for (const resourceKey of Object.keys(resources)) {
