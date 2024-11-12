2bfc72c89e9f1818c3297adb4e462f93f7e6be7d
Matthew Mihok <mihok@identbase.io>
2024-11-12 07:11:47 -0500

    feat: Adding delegate persona functionality

---
+++ src/resources/Persona.js
@@ -0,0 +1,34 @@
+import { API_BASE_URL } from "./consts";
+export default {
+    delegatePersona: async function ({ accessToken, hash, hostname }) {
+        const request = fetch(`${API_BASE_URL}/v3/persona/${hash}/delegate`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({
+                hostname,
+            }),
+        });
+        let response = null;
+        try {
+            response = await request;
+        }
+        catch (err) {
+            return Promise.reject({
+                error: 'InternalServerError',
+                message: err,
+                status: response?.status,
+            });
+        }
+        if (response?.ok) {
+            return Promise.resolve(await response.json());
+        }
+        else {
+            return Promise.resolve(false);
+        }
+    },
+};
+//# sourceMappingURL=Persona.js.map
---
+++ src/resources/Persona.ts
@@ -0,0 +1,44 @@
+import { API_BASE_URL } from "./consts";
+
+type ApiRequest = {
+  accessToken: string,
+}
+
+type DelegetePersonaByHostname = {
+  hash: string,
+  hostname: string,
+}
+
+export default {
+  delegatePersona: async function ({ accessToken, hash, hostname }: ApiRequest & DelegetePersonaByHostname) {
+    const request = fetch(`${API_BASE_URL}/v3/persona/${hash}/delegate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        hostname,
+      }),
+    });
+
+    let response: Response | null = null;
+
+    try {
+      response = await request;
+    } catch (err) {
+      return Promise.reject({
+        error: 'InternalServerError',
+        message: err,
+        status: response?.status,
+      });
+    }
+
+    if (response?.ok) {
+      return Promise.resolve(await response.json());
+    } else {
+      return Promise.resolve(false);
+    }
+  },
+};
